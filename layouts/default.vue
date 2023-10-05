8d58be15d3e7a8a0d9dfbc62ffabe7b01dd07a7d
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-10-04 21:05:23 -0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+  <header class="shadow-sm bg-white">
+    <nav class="container mx-auto p-4 flex justify-between ">
+        <nuxt-link class="font-bold" to="/">hundred</nuxt-link>
+        <ul class="flex gap-4">
+            <nuxt-link to="/about">about</nuxt-link>
+            <nuxt-link to="/products">products</nuxt-link>
+        </ul>
+        
+    </nav >
+  </header>
+    </div>
+
+    <div class="container mx-auto p-4">
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div v-if="slide" class="slide">
+            <Slide/>
+        </div>
+        <div v-else>
+            home    
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref , onMounted} from 'vue';
+import Slide from '~/components/Slide.vue';
+  
+const slide = ref(true)
+
+
+onMounted(() => {
+    setInterval(() => {
+
+slide.value =false
+
+}, 4000)
+
+
+})
+
+</script>
+
+<style scoped>
+
+</style>
